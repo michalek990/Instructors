2876aa3fca0c79a8c6626577666b58de2ca3076e
mgrzeszuk <m.grzeszuk@il-pib.pl>
2024-02-21 11:35:40 +0100

    Add swagger and swagger bearer authorization

--- src/main/java/com/example/instructors/Config/SwaggerConfig.java
+++ src/main/java/com/example/instructors/Config/SwaggerConfig.java
@@ -2,9 +2,31 @@ package com.example.instructors.Config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Docker Spring Rest API", version = "v1.0"))
+@OpenAPIDefinition(info = @Info(title = "Spring Rest API for Instructors", version = "v1.0"))
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
 }
+
