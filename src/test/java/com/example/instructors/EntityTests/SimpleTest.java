44a7b2ca100856c767ae5501a92df28476b78f25
michalek990 <michal.grzeszuk@gmail.com>
2024-03-05 13:29:40 +0100

    add simple test and test folder

---
+++ src/test/java/com/example/instructors/EntityTests/SimpleTest.java
@@ -0,0 +1,19 @@
+package com.example.instructors.EntityTests;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+@SpringBootTest
+public class SimpleTest {
+
+    @Test
+    public void addNumberEqualFour(){
+        int a = 2;
+        int b = 2;
+        assertEquals("Sum should be 4", 4, a+b);
+    }
+
+}
