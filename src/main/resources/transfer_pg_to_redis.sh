5146d540ae2585f352302a99885e066e6fe7c16f
michalek990 <93988101+michalek990@users.noreply.github.com>
2024-04-01 22:48:27 +0200

    Add bash script to transfer data from postgres database to redis database. TODO: implement to run script with spring application.

--- src/main/resources/transfer_pg_to_redis.sh
+++ src/main/resources/transfer_pg_to_redis.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-# Parametry połączenia PostgreSQL
 PGHOST="localhost"
-PGPORT="5434"
-PGDATABASE="test"
+PGPORT="5432"
+PGDATABASE="instructorsDb"
 PGUSER="postgres"
 PGPASSWORD="postgres"
 
-# Parametry połączenia Redis
 REDIS_HOST="localhost"
 REDIS_PORT="6379"
 
-# Zapytanie SQL do pobrania danych produktów
 SQL="SELECT id, name, price FROM products;"
 
-# Eksport danych z PostgreSQL i przetwarzanie każdego wiersza
 export PGPASSWORD=$PGPASSWORD
 psql -h $PGHOST -p $PGPORT -d $PGDATABASE -U $PGUSER -t -A -F"," -c "$SQL" | while IFS=, read -r id name price
 do
-  # Wstawianie danych do Redis za pomocą redis-cli
-  redis-cli -h $REDIS_HOST -p $REDIS_PORT HMSET product:$id name "$name" price "$price"
-done
+  redis-cli -h $REDIS_HOST -p $REDIS_PORT HMSET products:$id name "$name" price "$price"
+  redis-cli -h $REDIS_HOST -p $REDIS_PORT SADD products $id
 
+done
 echo "Udalo sie"
