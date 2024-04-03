96d398a5ae0cfb6a620950203188f6aebf3ad2bc
michalek990 <michal.grzeszuk@gmail.com>
2024-04-03 11:33:41 +0200

    Implement method to find city by name.

--- src/main/java/com/example/instructors/City/CityRepository.java
+++ src/main/java/com/example/instructors/City/CityRepository.java
@@ -4,6 +4,9 @@ import com.example.instructors.Entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
+    Optional<City> findByName(String cityName);
 }
